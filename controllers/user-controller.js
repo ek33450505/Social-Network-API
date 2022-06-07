06fa7e943779a33901948f4915791bde52a9d332
Ed Kubiak <ek33450505@gmail.com>
2022-06-07 13:00:24 -0400

    fixed package.json and added some route data

---
+++ controllers/user-controller.js
@@ -0,0 +1,32 @@
+const { User } = require('../models');
+
+const UserController = {
+    // get all users
+    getAllUser(req, res) {
+      User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+  
+    // get one user by id
+    getUserById({ params }, res) {
+      User.findOne({ _id: params.id })
+        .then(dbUserData => {
+          // If no user is found, send 404
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+  }
+
+module.exports = userController;
